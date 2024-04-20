076f5f8a9d73e531995a6ad2b6b2fdb1209cbf50
cynic <kurisufag1@gmail.com>
2024-04-20 19:30:09 -0400

    hashtag do better sweaty

--- client/volume.py
+++ client/volume.py
@@ -1,16 +1,21 @@
-import time, os
+import time, subprocess, dbus
 from core import *
 
 s = setup("/dev/ttyACM0")
 state = {}
 lastvol = update(s, state)["vol"]
 
+interface = dbus.Interface(dbus.SessionBus().get_object('org.Cinnamon', '/org/Cinnamon'), 'org.Cinnamon')
+
 while True:
     state = update(s, state)
     if abs(state["vol"] - lastvol) >= 10:
-        os.system("amixer -D pulse sset Master "+str(state["vol"]/1024*100)+"%")
-        print("changed", str(state["vol"]/1024*100))
-        print("changed", str(state["vol"]))
         lastvol = state["vol"]
+        adjusted = int(state["vol"]/1024*100)
+        subprocess.call(f"pactl set-sink-volume @DEFAULT_SINK@ {adjusted}%", shell=True)
+
+        level = ["muted", "low", "medium", "high"][int(state["vol"]/1024*3 + .5)]
+        logo = f"audio-volume-{level}-symbolic"
+        interface.ShowOSD({"icon": logo, "level": adjusted})
 
 unsetdown(s)
