f4492fce9763c5293f148a2495ec0c0c6a87f789
cynic <kurisufag1@gmail.com>
2024-04-20 22:13:11 -0400

    abstract packet creation

--- arduino/volume/volume.ino
+++ arduino/volume/volume.ino
@@ -3,10 +3,18 @@ void setup() {
   Serial.begin(500000);
 }
 
+String formpacket(String name, String value)
+{
+  String packet = "";
+  packet.concat(name);
+  packet.concat(":");
+  packet.concat(value);
+  packet.concat(";");
+  return packet;
+}
+
 void loop() {
   String packet = "^";
-  packet.concat("vol:");
-  packet.concat(String(analogRead(A0), DEC));
-  packet.concat(";");
+  packet.concat(formpacket("vol", String(analogRead(A0), DEC)));
   Serial.println(packet);
 }
